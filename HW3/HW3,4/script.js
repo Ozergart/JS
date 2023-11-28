94075e699de1da82a13aeeec1267c31a46398228
Kiril <ozergart@gmail.com>
2023-11-29 00:09:31 +0300

    HW3

---
+++ HW3/HW3,4/script.js
@@ -0,0 +1,74 @@
+let arr = [2,17,13,6,22,31,45,66,100,-18];
+
+let i = 0;
+// while (i<arr.length){
+//     const numb = arr[i];
+//     console.log(numb);
+//     i++;
+// }
+
+// for (let i = 0; i < arr.length; i++ ){
+//     const numb = arr[i];
+//     console.log(numb);
+// }
+
+// i = 0
+// while (i<arr.length){
+//     const numb = arr[i]
+//     if(i%2 !== 0 ){
+//         console.log(numb);
+//     }
+//     i++
+// }
+
+// for(i = 1; i<arr.length;i+=2){
+//     console.log(arr[i])
+// }
+
+// i = 0;
+// while (i<arr.length){
+//     const numb = arr[i];
+//     if(numb%2 ===0){
+//         console.log(numb);
+//     }
+//     i++
+// }
+
+// for(i=0;i<arr.length;i++){
+//     if(arr[i]%2===0){
+//         console.log(arr[i])
+//     }
+// }
+
+// for(i=0;i<arr.length;i++){
+//     if(arr[i]%3 ===0){
+//         arr[i] = 'okten';
+//     }
+// }
+// console.log(arr)
+
+// for(i=arr.length-1;i>-1;i--){
+//     console.log(arr[i])
+// }
+
+let arr2 =['sdgde', 'ferfefefer', 'fwerfewfef', 'fewrfwefewrf', 'wefwefew', 'fewrfewf', true, false, 23544, 23, 234, 1234, 12, false, 234, true]
+
+// for (i=0;i<arr2.length;i++){
+//     const numb = arr2[i]
+//     if (typeof numb === 'string'){
+//         console.log(numb);
+//     }
+// }
+// for (i=0;i<arr2.length;i++){
+//     const numb = arr2[i]
+//     if (typeof numb === 'number'){
+//         console.log(numb);
+//     }
+// }
+// for (i=0;i<arr2.length;i++){
+//     const numb = arr2[i]
+//     if (typeof numb === 'boolean'){
+//         console.log(numb);
+//     }
+// }
+
