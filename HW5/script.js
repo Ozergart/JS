9b8f3597c1f063893ee7b2401a293d9579b0b1ad
Kiril <ozergart@gmail.com>
2023-12-05 15:00:39 +0300

    HW5

--- HW5/script.js
+++ HW5/script.js
@@ -14,7 +14,7 @@ const area_of_cilindre = (h, r) => (2 * Math.PI * r * h) + (2 * Math.PI * r * r)
 console.log(area_of_cilindre(12, 4));
 
 // - створити функцію яка приймає масив та виводить кожен його елемент
-let iterator =  function (arr){
+let iterator =  (arr) => {
     for (const arrElement of arr) {
         console.log(arrElement);
     }
@@ -27,7 +27,7 @@ const par = (text) => document.write(`<p> ${text} </p>`)
 par('Lorem ipsum dolor.');
 
 // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
-function ul_creating3(text){
+const ul_creating3 = (text) => {
     document.write('<ul>');
     document.write(`<li> ${text} </li>`);
     document.write(`<li> ${text} </li>`);
@@ -37,18 +37,18 @@ function ul_creating3(text){
 ul_creating3('Lorem ipsum dolor.');
 
 // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
-function ul_creating_x(text, x){
+const ul_creating_x = (text, x) => {
     document.write('<ul>');
-        for (let i = 0;i < x; i++){
-            document.write(`<li> ${text} </li>`)
-        }
+    for (let i = 0; i < x; i++) {
+        document.write(`<li> ${text} </li>`)
+    }
     document.write('</ul>');
 }
-ul_creating_x(234234,12);
+ul_creating_x('Shadows',12)
 
 // - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
 
-function ul_builder(arr){
+const ul_builder = (arr)=> {
     document.write('<ul>');
         for (let arrElement of arr) {
             document.write(`<li>${arrElement}</li>`)
@@ -59,7 +59,7 @@ function ul_builder(arr){
 ul_builder(ReginaRaut);
 
 // - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
-function arr_sorting(arr){
+const arr_sorting = (arr) =>{
     for (const arrElement of arr) {
         document.write('<div>');
         for (const arrElementKey in arrElement) {
@@ -72,7 +72,7 @@ let LisaShu = [{id: 0, name: 'HaHuynh', age: 42 },{id: 1, name: 'UshaCaudhari', 
 arr_sorting(LisaShu);
 
 // - створити функцію яка повертає найменьше число з масиву
-function min_number(arr){
+const min_number = (arr) =>{
     let result = arr[0];
     for (let i = 0; i < arr.length; i++) {
         const arrElement = arr[i];
@@ -80,36 +80,35 @@ function min_number(arr){
             result = arrElement;
         }
     }
-    document.write(result);
-    return result
+   console.log(result);
 }
-let PingZhong = [100, 54, 12, 654, 234, 13, 456, 44, 876];
+let PingZhong = [100, 54, 13, 654, 234, 13, 456, 44, 878];
 min_number(PingZhong);
 
 // - створити функцію sum(arr)яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад sum([1,2,10]) //->13
-function sum(arr) {
+const sum = (arr) => {
     let resultsum = 0;
     for (const arrElement of arr) {
         resultsum += arrElement;
     }
     document.write(`<div>${resultsum}</div>`);
-    return resultsum;
 }
 sum(PingZhong)
 // - створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відаовідних індексах
 // Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
-function swap(arr,i1,i2){
+const swap = (arr,i1,i2) =>{
     let reserved = arr[i1]
     arr[i1] = arr[i2]
     arr[i2] = reserved
     document.write(arr)
 }
 let SalehBao =[11,22,33,44];
-swap(SalehBao,0,1)
+swap(SalehBao,1,2)
 // - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
 // Приклад exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD') // => 250
+
    let currences = [{currency:'USD',value:40},{currency:'EUR',value:42}];
-function exchange(sumUAH,currencyValues,exchangeCurrency){
+const exchange = (sumUAH,currencyValues,exchangeCurrency) => {
     let exchange_value = 0;
     for (const currency of currencyValues) {
         if (currency.currency === exchangeCurrency ){
@@ -119,8 +118,8 @@ function exchange(sumUAH,currencyValues,exchangeCurrency){
     let result = sumUAH / exchange_value
     document.write(`<div>${result}</div>`);
 }
-exchange(100000,currences, 'EUR');
+exchange(10000,currences, 'EUR');
 document.write('<div>----------------------------</div>')
-exchange(100000,currences, 'USD');
+exchange(10000,currences, 'USD');
 
 
